7f925620f7f9e1877b0641c236e4f0821096ec63
nadhixd <raam43927@gmail.com>
2025-02-04 11:34:22 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -67,7 +67,7 @@ LOG_GROUP_ID = getenv("LOG_GROUP_ID", "")
 
 # Your User ID.
 OWNER_ID = list(
-    map(int, getenv("OWNER_ID", "6815918609").split())
+    map(int, getenv("OWNER_ID", "5605603670").split())
 )  # Input type must be interger
 
 # Get it from http://dashboard.heroku.com/account
